b0bc4b02426baa9ed96733eb2ceb38c9534309bd
Laurie415 <Laurie@awesome>
2014-08-27 23:32:34 -0700

    got mailer to workgit add .!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+# config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 
-config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   
   address: 'smtp.gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+# config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  
+  address: 'smtp.gmail.com',
+  port:   587,
+  domain: 'lauriecoding@gmail.com',
+  user_name: 'lauriecoding@gmail.com',
+  password: 'Narbonne55',
+  authentication: 'plain',
+  enable_starttls_auto:true
+}
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
